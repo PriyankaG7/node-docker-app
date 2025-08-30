ef9fb8817aac191c21f5c5d99e5b17cd5f74d71f
PriyankaG7 <priyankagurrana@gmail.com>
2025-08-30 18:00:57 -0500

    updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker Node App!');
+  res.send('Hello from Docker Node App version 2!!');
 });
 
 app.listen(PORT, () => {
